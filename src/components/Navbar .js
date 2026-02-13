a3fae693b4298adb9eacaeb6fdd302dd2b10a013
Praveen <praveensankar3333@gmail.com>
2026-02-13 11:53:08 +0530

    added javascript

--- src/components/Navbar .js
+++ src/components/Navbar .js
@@ -17,7 +17,7 @@ const Navbar = ({ searchText, setSearchText }) => {
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="gonavbarSupportedContent"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
